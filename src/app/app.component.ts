3dce89458ebc93dbe0bfe559fd127f81ea14fdfd
BassamMuhammad <72605456+BassamMuhammad@users.noreply.github.com>
2021-05-26 20:34:45 +0500

    simple todo-list in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-todo-list';
+  public tasks: string[] = [];
+
+  addTask(task: string) {
+    task ? this.tasks.push(task) : alert('Please enter the text of task');
+  }
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
 }
